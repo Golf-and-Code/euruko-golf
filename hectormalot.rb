fb2c2224525e09b3026202ccb3867f0c1c97ddf5
Dennis <dennisdereus@gmail.com>
2012-05-07 14:58:06 +0300

    small cleanup

--- hectormalot.rb
+++ hectormalot.rb
@@ -1,3 +1,3 @@
 # encoding: utf-8
 # Twitter: https://twitter.com/#!/HectorMalot/status/199242822178705409
-n,s,r=0,->s{print(s)},->i{i==0?s['_|']:s['  ']};"ￏ劜抐劜刐".codepoints{|c|16.times{|i|r[c%2];c=c>>1;n+=1;s["\n"]if n%32==0}}
+n,s,r=0,->s{print(s)},->i{i==0?s['_|']:s['  ']};"ￏ劜抐劜刐".codepoints{|c|16.times{r[c%2];c=c>>1;n+=1;s["\n"]if n%32==0}}
