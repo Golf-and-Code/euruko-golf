8b1443828ae68b3819b96e805e8272369dfe862c
Hans Hasselberg <me@hans.io>
2012-05-07 14:31:47 +0200

    Add a description.

--- i0rek.rb
+++ i0rek.rb
@@ -1,3 +1,29 @@
+# Yo dawg, I heard you like ascii art.
+# So I put ascii art in your code so you can see ascii art
+# while you code ascii art.
+#
+# When you execute this script 'euruko' is printed in ascii art.
+#
+# $ ruby i0rek.rb
+# " ### # # ##  # # # #  # "
+# " #   # # # # # # # # # #"
+# " ##  # # ##  # # ##  # #"
+# " ### ### # # ### # #  # "
+#
+# The funny thing is, that this script itself is ascii art already!
+#
+# Don't see what I mean? Let me help you by replacing some chars with #:
+#
+# eval"
+#
+# ### # # ##  # # e w  W
+# #   # # # # # # c k ( N
+# ##  # # ##  # # ma  p {
+# ### ### # # ##0 1 N  ,
+#
+# '=#'}".tr(" \n","").tr("=NMX"," '.|")
+#
+# Hope you enjoyed it!
 eval"
 
 Nue R E UU  e e e w  W
