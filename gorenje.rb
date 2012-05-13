429a4bc34158abf838cc361fb5086823031478f6
Gerrit Riessen <gerrit.riessen@gmail.com>
2012-05-13 22:36:43 +0200

    too long

--- gorenje.rb
+++ gorenje.rb
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-p=->c{(0..22).map{|a|c[a]}};p[->x{p[->y{print "\e[3%dm💚 \e[0m"%[7,0][("0"+"\u{0}🲧󲂚򂗗󗒺񒺗񝗒ꊂ񿕟󀀨ॖ򴅅𧨉񈲁򆆐񷌍̺󧼗󸠢ॵ󿸮򛇥󑕀򂾀🰭񐀀".codepoints.map{|a|"%20s"%a.to_s(2)}.join)[(x*23)+y].to_i]}];puts}]
+r=(0..22);r.map{|x|r.map{|y|print"\e[3%dm💚 \e[0m"%[7,0][("0"+"\u{0}🲧󲂚򂗗󗒺񒺗񝗒ꊂ񿕟󀀨ॖ򴅅𧨉񈲁򆆐񷌍̺󧼗󸠢ॵ󿸮򛇥󑕀򂾀🰭񐀀".codepoints.map{|a|"%20s"%a.to_s(2)}.join)[(x*23)+y].to_i]};p''}
 
