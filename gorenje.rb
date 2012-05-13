e5e39be8c07d9247dfd5637d6bc1f12b087de82e
Gerrit Riessen <gerrit.riessen@gmail.com>
2012-05-13 22:52:08 +0200

    4 chars too long

--- gorenje.rb
+++ gorenje.rb
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-(0..528).map{|x|print"\e[3%dm💚 \e[0m"%[7,0][("0"+"\0🲧󲂚򂗗󗒺񒺗񝗒ꊂ񿕟󀀨ॖ򴅅𧨉񈲁򆆐񷌍̺󧼗󸠢ॵ󿸮򛇥󑕀򂾀🰭񐀀".codepoints.map{|a|"%20s"%a.to_s(2)}.join)[x].to_i];x%23==22?p(''):nil}
+529.times{|x|print"\e[3%dm💚 \e[0m"%[7,0][("0"+"\0🲧󲂚򂗗󗒺񒺗񝗒ꊂ񿕟󀀨ॖ򴅅𧨉񈲁򆆐񷌍̺󧼗󸠢ॵ󿸮򛇥󑕀򂾀🰭񐀀".codepoints.map{|a|"%20s"%a.to_s(2)}.join)[x].to_i];x%23==22?p(''):x}
 
