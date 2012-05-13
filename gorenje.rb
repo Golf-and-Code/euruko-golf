6b0f630645e0ea93060de03b95c321c8523403c5
Gerrit Riessen <gerrit.riessen@gmail.com>
2012-05-13 14:02:09 +0200

    even longer

--- gorenje.rb
+++ gorenje.rb
@@ -1,1 +1,2 @@
-p=->(a){putc a;sleep 0.1};l=[' ','#',"\n"];(0..123).each{|d|['.','o','.'].each{|c|p.call c;putc "\b"};p.call l["87lpcdpfmaol0fzag94jwwzdotmhkrjb3gi7w".to_i(36).to_s(3)[d].to_i]}
+# -*- coding: utf-8 -*-
+l=["\e[37m██\e[0m","\e[30m██\e[0m","\n"];(0..552).each{|d|print l["4jcwz2qe3w75q05luoqjspbll4d1fns9c7upthes4nui6r6ibxy0rd9z41ijhh844witjexk0hswm5qbw28nosk8go3zvjr61bx7cfrz9hxh010px5zbz8vvwupny2820yl96zhtd94qqxg4j839j2taou5w652nn77f8t1emk".to_i(36).to_s(3)[d].to_i]}
