c458318243d11e62d1eb061455c36756b39ee257
Gerrit Riessen <gerrit.riessen@gmail.com>
2012-05-13 23:11:10 +0200

    made it

--- gorenje.rb
+++ gorenje.rb
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-529.times{|x|print"\e[3%dm💚 \e[0m"%[7,0][("0"+"\0🲧󲂚򂗗󗒺񒺗񝗒ꊂ񿕟󀀨ॖ򴅅𧨉񈲁򆆐񷌍̺󧼗󸠢ॵ󿸮򛇥󑕀򂾀🰭񐀀".codepoints.map{|a|"%20s"%a.to_s(2)}.join)[x].to_i];x%23==22?p(''):x}
+529.times{|x|print"\e[3%dm💚 \e[0m"%[7,0][("\0﹓󹁍񁋫󫩝𩝋򮫩允𿪯󠀔ҫ񚂢򓴄򤙀󃃈𻦆򀆝񳾋󼐑Һ󿼗񍣲󨪠񁟀︖򨀀".codepoints.map{|a|"%20s"%a.to_s(2)}.join)[x].to_i];x%23==22?p(''):x}
 
